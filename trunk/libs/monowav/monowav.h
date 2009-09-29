c0e935dbbd5d25fa616b7e12ea66596bff14f03e
percival.music.ca <percival.music.ca@d9492fb8-bd30-11dd-824f-6f77523b18d3>
2009-09-29 20:13:03 +0000

    Finish monowav "library".  Sweet mao I feel like an idiot.

--- trunk/libs/monowav/monowav.h
+++ trunk/libs/monowav/monowav.h
@@ -1,4 +1,5 @@
 int monowav_write(const char *filename, const short *buffer,
                   const unsigned long bufferLength);
-
+int monowav_read(const char *filename, short *buffer,
+                 unsigned long *bufferLength);
 
