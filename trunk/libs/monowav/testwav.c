f365356e9000a77f2363a2422c5673dae6a372b8
percival.music.ca <percival.music.ca@d9492fb8-bd30-11dd-824f-6f77523b18d3>
2009-09-29 19:51:03 +0000

    finally working.

--- trunk/libs/monowav/testwav.c
+++ trunk/libs/monowav/testwav.c
@@ -2,16 +2,18 @@
 #include <math.h>
 #include <limits.h>
 
+#define BUFSIZE 100
 int main()
 {
-	short buffer[2048];
+	short buffer[BUFSIZE];
 	unsigned long i;
-	for (i=0; i<2048; i++)
+	for (i=0; i<BUFSIZE; i++)
 	{
 //		buffer[i] = INT_MAX * sin(i);
-		buffer[i] = SHRT_MAX * ((float) i / 2048.0);
+		buffer[i] = SHRT_MAX * ((float) i / (float)BUFSIZE);
+//		buffer[i] = 0;
 	}
-	monowav_write("foo.wav", &buffer, 2048);
+	monowav_write("foo.wav", &buffer, BUFSIZE);
 
 	return 0;
 }
