910432148a8f1a4d78ceb31811da00352f966be7
percival.music.ca <percival.music.ca@d9492fb8-bd30-11dd-824f-6f77523b18d3>
2009-09-30 13:47:08 +0000

    Currently not working (a garbage undefined reference)

--- libs/monowav/monowav.h
+++ libs/monowav/monowav.h
@@ -1,3 +1,6 @@
+#define MONOWAV_OK 0
+#define MONOWAV_ERROR 1
+
 int monowav_write(const char *filename, const short *buffer,
                   const unsigned long bufferLength);
 int monowav_read(const char *filename, short *buffer,
